c03dab815e64c81dc88d3b1d3e84253c819576a2
miguelo <alexanderchangcruz11@gmail.com>
2025-11-07 02:10:38 -0500

    feat add my ms-config-server

---
+++ src/main/java/com/synopsis/config/MsConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.synopsis.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class MsConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsConfigServerApplication.class, args);
+	}
+
+}
